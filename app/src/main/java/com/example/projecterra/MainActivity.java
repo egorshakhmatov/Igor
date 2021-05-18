126a5dc5a2e969fb48ec072a5e6b54e71aa8198d
egorshakhmatov <shakhmatov_zhora@mail.ru>
2021-05-18 19:14:54 +0700

    12

--- app/src/main/java/com/example/projecterra/MainActivity.java
+++ app/src/main/java/com/example/projecterra/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//1231231234
+//1231231234 *****
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
