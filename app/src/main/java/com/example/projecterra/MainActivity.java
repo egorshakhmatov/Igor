ea84fe8109471fed0b038ab62191ae464b5cf08c
egorshakhmatov <shakhmatov_zhora@mail.ru>
2021-05-18 19:09:24 +0700

    q

---
+++ app/src/main/java/com/example/projecterra/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.projecterra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//1231231234
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
